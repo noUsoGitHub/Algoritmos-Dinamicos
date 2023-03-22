ee03554cd838f919ccf82f27c37194739ffc088a
noUsoGitHub <89752129+noUsoGitHub@users.noreply.github.com>
2023-03-22 17:24:53 -0500

    añadido tabulado
    se añadio el tabulado a los algoritmos que se tenian

---
+++ Codigos/Tabulado.py
@@ -0,0 +1,26 @@
+from speedCommons import pretty
+def bottomUp(sec,M):
+    for i in range(len(sec)):
+        for j in range(len(sec)):
+            max_product = 1
+            for k in range(i,j+1):
+                if sec[k]!=0:
+                    max_product *= sec[k]
+                else:
+                     break
+            M[i][j] = int(max_product)
+    return M
+def subSequence(X, memo):
+        max_val = memo[0][len(X)-1]
+        i, j = 0, len(X)-1
+        for x in range(len(X)):
+            for y in range(x, len(X)):
+                if memo[x][y] == max_val:
+                    i, j = x, y
+                    break
+        return X[i:j+1]
+def rec(seq):
+        M = [[float('-inf') for i in range(len(seq))] for j in range(len(seq))]
+        bottomUp(seq,M)
+        #pretty.prettyMatrix(M) # Unicamente para ver la matriz
+        return subSequence(seq, M) # La subsecuencia
